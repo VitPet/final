4d972844238dc7dc3a23c324f8902492bb4b4c86
VitPet <petrusenkovitaly@mail.ru>
2017-02-03 00:28:19 +0200

    fixed js

--- app.js
+++ app.js
@@ -188,20 +188,6 @@ You no longer need a desktop or laptop to be connected via VK. Install our offic
 			}
 		});
 
-    $(document).ready(function() {
-        var RBStickyNav = $('.RBsticky-element').offset().top;
-        var stickyNav = function() {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > RBStickyNav) {
-                $('.RBsticky-element').addClass('RBSticky');
-            } else {
-                $('.RBsticky-element').removeClass('RBSticky');
-            }
-        };
-        stickyNav();
-        $(window).scroll(function() {
-            stickyNav();
-        });
-    });
+   
  
 	
